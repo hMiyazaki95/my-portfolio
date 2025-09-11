20101162b41b593c1972d203306358d3ca312b7a
hMiyazaki95 <54996878+hMiyazaki95@users.noreply.github.com>
2025-09-11 10:00:33 +0100

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,18 +1,37 @@
-/** @type {import('next').NextConfig} */
+///** @type {import('next').NextConfig} */
 // const nextConfig = {
 //   reactStrictMode: true,
 // };
 
 // export default nextConfig;
 
+// const repo = 'my-portfolio';
+// const usingCustomDomain = true; // set to true if you have CNAME
+
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {
+//   reactStrictMode: true,
+//   output: 'export',
+//   images: { unoptimized: true },
+//   trailingSlash: true,
+//   ...(usingCustomDomain ? {} : { basePath: `/${repo}`, assetPrefix: `/${repo}/` }),
+// };
+
+// export default nextConfig;
+
+
+
+// next.config.mjs (ESM)
 const repo = 'my-portfolio';
-export default {
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
   reactStrictMode: true,
   output: 'export',
   images: { unoptimized: true },
   trailingSlash: true,
   basePath: `/${repo}`,
   assetPrefix: `/${repo}/`,
-}
+};
 
 export default nextConfig;
